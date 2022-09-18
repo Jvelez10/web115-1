ac058f5112be986150c472294f1b7ba192912c53
cbaile47 <111405246+cbaile47@users.noreply.github.com>
2022-09-18 16:29:39 -0400

    Updates

--- scripts/fizzbuzz0.js
+++ scripts/fizzbuzz0.js
@@ -6,4 +6,12 @@ function greetChange() {
 	let lastName = document.getElementById('lName').value;
 	let newGreet = `Welcome, ${firstName} ${midInitial} ${lastName}!`
 	document.getElementById('greeting').innerHTML = newGreet;
+	
+	let countMax = prompt(`How high do you want to count, ${firstName}?`)
+	let result = '';
+	for (var i = 1; i <= countMax; i++) {
+		i % 2 === 0 ? result += "Web Spun - the number is even<br>" : result += "Web Spun - the number is odd<br>";
+	}
+	
+	document.getElementById('output').innerHTML = result;
 }
