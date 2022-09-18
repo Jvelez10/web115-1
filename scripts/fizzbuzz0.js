d2422368d28461858d4e6008c3fcc1b7c1d205bd
cbaile47 <111405246+cbaile47@users.noreply.github.com>
2022-09-18 15:40:57 -0400

    Updates

--- scripts/fizzbuzz0.js
+++ scripts/fizzbuzz0.js
@@ -1,2 +1,9 @@
 "use strict";
 
+function greetChange() {
+	let firstName = document.getElementById('fName').value;
+	let midInitial = document.getElementById('mName').value;
+	let lastName = document.getElementById('lName').value;
+	let newGreet = `Welcome, ${firstName} ${midInitial} ${lastName}!`
+	document.getElementById('greeting').innerHTML = newGreet;
+}
